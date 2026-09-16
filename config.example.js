1f416231382252715f04874989afad6a7d67e629
unknown <raymondpurba19@gmail.com>
2026-09-16 10:43:31 +0700

    Simplify config.example.js

--- config.example.js
+++ config.example.js
@@ -1,3 +1,2 @@
-// Salin file ini menjadi "config.js" lalu isi dengan API key kamu sendiri.
-// config.js tidak akan ikut ter-commit ke git (lihat .gitignore).
+
 const API_KEY = "GANTI_DENGAN_API_KEY_OPENWEATHERMAP_KAMU";
